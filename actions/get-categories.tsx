9193dd5b24299f2e942e15b3a3e686a2a320e3c9
JesusC-R <cuevasrodartej@gmail.com>
2023-07-30 21:32:12 -0700

    Feat: Add navbar.

--- actions/get-categories.tsx
+++ actions/get-categories.tsx
@@ -3,9 +3,17 @@ import { Category } from "@/types";
 const URL = `${process.env.NEXT_PUBLIC_API_URL}/categories`;
 
 const getCategories = async (): Promise<Category[]> => {
-  const res = await fetch(URL);
+  try {
+    const res = await fetch(URL);
+    if (!res.ok) {
+      throw new Error("Network response was not ok.");
+    }
 
-  return res.json();
-}
+    return res.json();
+  } catch (error) {
+    console.error("Error fetching categories:", error);
+    throw error;
+  }
+};
 
 export default getCategories;
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -3,9 +3,11 @@ import Link from "next/link";
 import MainNav from "@/components/main-nav";
 import Container from "@/components/ui/container";
 import getCategories from "@/actions/get-categories";
+import NavbarActions from "./navbar-actions";
+
+export const revalidate = 0; // Allows to load data without hard reload.
 
 const Navbar = async () => {
-
   const categories = await getCategories();
 
   return (
@@ -13,15 +15,14 @@ const Navbar = async () => {
       <Container>
         <div className="relative px-4 sm:px-6 lg:px-8 flex h-16 items-center">
           <Link href="/" className="ml-4 flex lg:ml-0 gap-x-2">
-            <p className="font-bold text-xl">
-              STORE
-            </p>
+            <p className="font-bold text-xl">STORE</p>
           </Link>
-          <MainNav data={categories}/>
+          <MainNav data={categories} />
+          <NavbarActions />
         </div>
       </Container>
     </div>
   );
-}
+};
 
-export default Navbar
+export default Navbar;
