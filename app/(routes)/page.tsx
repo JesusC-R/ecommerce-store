62bb09629f067d89c146f3972f5690d62f9f4cfa
JesusC-R <cuevasrodartej@gmail.com>
2023-07-31 23:00:10 -0700

    Refactor: Improved readability.

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -7,19 +7,19 @@ import ProductList from "@/components/product-list";
 export const revalidation = 0;
 
 const HomePage = async () => {
-  const featuredProducts = await getProducts({ isFeatured:true });
+  const featuredProducts = await getProducts({ isFeatured: true });
   const billboard = await getBillboard("c3ad251a-0e56-48a6-842e-7945f75be8db");
 
   return (
-    <div className="space-y-10 pb-10">
-      <Container>
-        <Billboard data={billboard}/>
-      </Container>
-      <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
-        <ProductList title="Featured Products" items={featuredProducts}/>
+    <Container>
+      <div className="space-y-10 pb-10">
+        <Billboard data={billboard} />
+        <div className="flex flex-colgap-y-8 px-4 sm:px-6 lg:px-8">
+          <ProductList title="Featured Products" items={featuredProducts}/>
+        </div>
       </div>
-    </div>
+    </Container>
   );
-}
+};
 
 export default HomePage;
