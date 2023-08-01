be262b755b69da088f86d8c14b66310a68b9aad6
JesusC-R <cuevasrodartej@gmail.com>
2023-07-31 22:54:34 -0700

    Feat: Add product cards.

--- components/product-list.tsx
+++ components/product-list.tsx
@@ -1,4 +1,6 @@
 import { Product } from "@/types";
+import NoResults from "@/components/ui/no-results";
+import ProductCard from "@/components/ui/product-card";
 
 interface ProductListProps {
   title: string;
@@ -6,12 +8,17 @@ interface ProductListProps {
 }
 
 const ProductList: React.FC<ProductListProps> = ({ title, items }) => {
-  return <div className="space-y-4">
-    <h3>{title}</h3>
-    {items.map((item, index) => (
-      <li key={index}>{item.name}</li>
-    ))}
-  </div>;
+  return (
+    <div className="space-y-4">
+      <h3 className="font-bold text-3xl">{title}</h3>
+      {items.length === 0 && <NoResults />}
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+        {items.map(item => (
+          <ProductCard key={item.id} data={item}/>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default ProductList;
