f41f45be224b0b1c175a478dc0daf83f68f0e98c
JesusC-R <cuevasrodartej@gmail.com>
2023-08-03 15:18:37 -0700

    Feat: Add Toast notifictions as a provider.

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import { Urbanist } from "next/font/google";
 import Footer from "@/components/footer";
 import Navbar from "@/components/navbar";
 import ModalProvider from "@/providers/modal-provider";
+import ToastProvider from "@/providers/toast-providers";
 
 const font = Urbanist({ subsets: ["latin"] });
 
@@ -20,8 +21,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={font.className}>  
+      <body className={font.className}>
         <ModalProvider />
+        <ToastProvider />
         <Navbar />
         {children}
         <Footer />
