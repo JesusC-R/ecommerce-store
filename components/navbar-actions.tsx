aecb97a69be587335b47d2bf0aef93238d0a2887
JesusC-R <cuevasrodartej@gmail.com>
2023-08-03 18:44:05 -0700

    Feat: Add checkout page.

--- components/navbar-actions.tsx
+++ components/navbar-actions.tsx
@@ -1,12 +1,34 @@
+"use client";
+
 import Button from "@/components/ui/button";
+import useCart from "@/hooks/use-cart";
 import { ShoppingCart } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { useState, useEffect } from "react";
 
 const NavbarActions = () => {
+  const [isMounted, setIsMounted] = useState(false);
+  const router = useRouter();
+  const cart = useCart();
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) {
+    return null;
+  }
+
   return (
     <div className="ml-auto flex items-center gap-x-4">
-      <Button className="flex items-center rounded-full bg-black px-4 py-2">
+      <Button
+        onClick={() => router.push("/cart")}
+        className="flex items-center rounded-full bg-black px-4 py-2"
+      >
         <ShoppingCart size={20} color="white" />
-        <span className="ml-2 text-sm font-medium text-white">0</span>
+        <span className="ml-2 text-sm font-medium text-white">
+          {cart.items.length}
+        </span>
       </Button>
     </div>
   );
